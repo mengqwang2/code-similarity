744e384ca34b70cf5869009a16edefeafecb2241
yanlzhang3@gmail.com <yanlzhang3@gmail.com@0cbcdd41-36aa-ad34-48df-35996ea58276>
2013-03-15 15:09:02 +0000

    checkSim() bug fixed

--- trunk/code-similarity/src/edu/hkcity/cs/FuncByFuncComparar.java
+++ trunk/code-similarity/src/edu/hkcity/cs/FuncByFuncComparar.java
@@ -116,48 +116,58 @@ public class FuncByFuncComparar extends Comparar{
 	      List<String> allMatches= new ArrayList<String>();
 	      while (m.find( )) {
 	    	  //System.out.print(m.group());
+	    	 
+	    	  
 	    	  allMatches.add(m.group());
 	      }
 	      return allMatches.toArray(new String[0]);
 	}
 
+	public double Pch(String tar,String org){
+		return checkSimilarity(tar,org);
 	
+	}
 	private double checkSimilarity(String tar, String org) {
 		  String[] token1 = getTok(tar);
 	      String[] token2 = getTok(org);
 	      int n=token1.length;
 	      int m=token2.length;
+	    
 	      int[][] C = new int[n+1][m+1];
 			
-	        /* C[i][0] = 0 for 0<=i<=n */
-	        for (int i = 0; i <= n; i++) {
-	            C[i][0] = 0;
-	        }
-		
-	        /* C[0][j] = 0 for  0<=j<=m */
-	        for (int j = 0; j <= m; j++) {
-	            C[0][j] = 0;
-	        }
-	        /* dynamic programming */
-	        for (int i = 1; i <= n; i++) {
-	            for (int j = 1; j <= m; j++) {
-	                if (token1[i-1]== token2[j-1]) 
-	                {
-	                    C[i][j]=C[i-1][j-1]+1;
-	                }
-	                else if (C[i-1][j]>=C[i][j-1]) 
-	                {
-	                    C[i][j]=C[i-1][j];
-	                }
-	                else 
-	                {
-	                    C[i][j]=C[i][j-1];     
-	                }
-	            }
-	        }
-	        return C[n][m]*1.0/n;
-	}
+		    /* C[i][0] = 0 for 0<=i<=n */
+		    for (int i = 0; i <= n; i++) {
+		    	C[i][0] = 0;
+		    }
+			
+		    /* C[0][j] = 0 for  0<=j<=m */
+		    for (int j = 0; j <= m; j++) {
+		        C[0][j] = 0;
+		    }
+		    /* dynamic programming */
+		    for (int i = 1; i <= n; i++) {
+		    	for (int j = 1; j <= m; j++) {
+		    		
+		    		if (token1[i-1].equals(token2[j-1])) 
+		            {	
+		    			C[i][j]=C[i-1][j-1]+1;
+		            }
+		            else if (C[i-1][j]>=C[i][j-1]) 
+		            {
+		            	C[i][j]=C[i-1][j];
+		            }
+		            else 
+		            {
+		            	C[i][j]=C[i][j-1];     
+		            }
+		        }
+		    }
+		    
+		    return C[n][m]*1.0/n;
+		}
 	
+	@SuppressWarnings("unused")
+
 	private double calSimilarity(double sims[], int lnt[]){
 		double interSim=0;
 		int totalLine=0;
